049d89f74ed40468ce1f0b27f93622bf680b6845
idrissi <idrissimo@yahoo.com>
2022-01-03 21:06:12 +0100

    First chapter + Rustup concepts

---
+++ hello_cargo/target/debug/deps/hello_cargo-5b1ebf27963c30fe.d
@@ -0,0 +1,5 @@
+/home/dev/Documents/Learning/PROGRAMMING/RUST/100DaysOfCode/hello_cargo/target/debug/deps/hello_cargo-5b1ebf27963c30fe: src/main.rs
+
+/home/dev/Documents/Learning/PROGRAMMING/RUST/100DaysOfCode/hello_cargo/target/debug/deps/hello_cargo-5b1ebf27963c30fe.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/dev/Documents/Learning/PROGRAMMING/RUST/100DaysOfCode/hello_cargo/target/debug/hello_cargo: /home/dev/Documents/Learning/PROGRAMMING/RUST/100DaysOfCode/hello_cargo/src/main.rs
